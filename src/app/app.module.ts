69e2eaae6ce63ad6db0caaa65c265b5d6d459dff
Rafael Freitas <oraffaudi@gmail.com>
2024-04-04 22:15:12 -0300

    created model module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { ModelModule } from './model/model.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ModelModule
   ],
   providers: [],
   bootstrap: [AppComponent]

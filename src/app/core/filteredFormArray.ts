030b413deed22f78cc0c3f07c5530175dd93f651
Rafael Freitas <oraffaudi@gmail.com>
2024-04-06 01:39:12 -0300

    improving cross-field validation

--- src/app/core/filteredFormArray.ts
+++ src/app/core/filteredFormArray.ts
@@ -1,9 +1,9 @@
-import { FormArray } from "@angular/forms";
+import { AbstractControl, FormArray } from "@angular/forms";
 
 export type ValueFilter = (value: any) => boolean;
 
 export class FilteredFormArray extends FormArray {
-    
+
     filter: ValueFilter | undefined = (val) => val == "" || val == null;
 
     _updateValue() {
@@ -12,4 +12,18 @@ export class FilteredFormArray extends FormArray {
                 (control.enabled || this.disabled) && !this.filter?.(control.value)
             ).map((control) => control.value);
     }
+
+    override push(control: AbstractControl,
+        options?: { emitEvent?: boolean | undefined; }): void {
+        super.push(control, options);
+        this.controls.forEach(c => c.updateValueAndValidity());
+    }
+    
+    override removeAt(index: number,
+        options?: { emitEvent?: boolean | undefined; }): void {
+        super.removeAt(index, options);
+        this.controls.forEach(c => c.updateValueAndValidity());
+    }
+
+
 }
--- src/app/validation/unique.ts
+++ src/app/validation/unique.ts
@@ -2,19 +2,49 @@ import { AbstractControl, FormArray, ValidationErrors, ValidatorFn }
     from "@angular/forms";
 
 export class UniqueValidator {
+
+    static uniquechild(control: AbstractControl): ValidationErrors | null {
+        return control.parent?.hasError("unique") ? { "unique-child": {} } : null;
+    }
+
     static unique(): ValidatorFn {
         return (control: AbstractControl): ValidationErrors | null => {
+            let badElems: AbstractControl[] = [];
+            let goodElems: AbstractControl[] = [];
+
             if (control instanceof FormArray) {
-                let badElems = control.controls.filter((child, index) => {
-                    return control.controls.filter((c, i2) => i2 != index)
+                control.controls.forEach((child, index) => {
+                    if (control.controls.filter((c, i2) => i2 != index)
                         .some(target => target.value != ""
-                            && target.value == child.value);
-                });
-                if (badElems.length > 0) {
-                    return { "unique": {} };
-                }
+                            && target.value == child.value)) {
+                        badElems.push(child);
+                    } else {
+                        goodElems.push(child);
+                    }
+                })
+                setTimeout(() => {
+                    badElems.forEach(c => {
+                        if (!c.hasValidator(this.uniquechild)) {
+                            c.markAsDirty();
+                            c.addValidators(this.uniquechild)
+                            c.updateValueAndValidity({
+                                onlySelf: true,
+                                emitEvent: false
+                            });
+                        }
+                    })
+                    goodElems.forEach(c => {
+                        if (c.hasValidator(this.uniquechild)) {
+                            c.removeValidators(this.uniquechild);
+                        }
+                        c.updateValueAndValidity({
+                            onlySelf: true,
+                            emitEvent: false
+                        })
+                    })
+                }, 0);
             }
-            return null;
+            return badElems.length > 0 ? { "unique": {} } : null;
         }
     }
 }

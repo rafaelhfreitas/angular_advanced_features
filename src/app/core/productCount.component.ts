e0e9da01447ad1c32fcdd182d468029cf73e151a
Rafael Freitas <oraffaudi@gmail.com>
2024-04-07 09:54:48 -0300

    accessing parameters from child routes

--- src/app/core/productCount.component.ts
+++ src/app/core/productCount.component.ts
@@ -2,6 +2,7 @@ import {
     Component, KeyValueDiffer, KeyValueDiffers, ChangeDetectorRef
 } from "@angular/core";
 import { Model } from "../model/repository.model";
+import { ActivatedRoute } from "@angular/router";
 
 
 @Component({
@@ -14,10 +15,22 @@ export class ProductCountComponent {
 
     private differ?: KeyValueDiffer<any, any>;
     count: number = 0;
-    
+    private category?: string;
+
     constructor(private model: Model,
         private keyValueDiffers: KeyValueDiffers,
-        private changeDetector: ChangeDetectorRef) { }
+        private changeDetector: ChangeDetectorRef,
+        activeRoute: ActivatedRoute) {
+
+            activeRoute.pathFromRoot.forEach( route => route.params.subscribe(
+                params => {
+                    if (params["category"] != null) {
+                        this.category = params["category"];
+                        this.updateCount();
+                    }
+                }
+            ))
+         }
     
     ngOnInit() {
         this.differ = this.keyValueDiffers
@@ -33,6 +46,9 @@ export class ProductCountComponent {
 
 
     private updateCount() {
-        this.count = this.model.getProducts().length;
+        // this.count = this.model.getProducts().length;
+        this.count = this.model.getProducts()
+            .filter(p => this.category == null || p.category == this.category)
+            .length;
     }
 }

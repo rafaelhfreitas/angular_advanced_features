84e73172f3a35cba21bd2e6e9cbc04c4de4786ad
Rafael Freitas <oraffaudi@gmail.com>
2024-04-07 09:17:00 -0300

    stylling links for active routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,11 +5,10 @@ import { NotFoundComponent } from "./core/notFound.component";
 
 
 const routes: Routes = [
-    // { path: "form/edit", component: FormComponent },
-    // { path: "form/create", component: FormComponent },
     { path: "form/:mode/:id", component: FormComponent },
     { path: "form/:mode", component: FormComponent },
     { path: "does", redirectTo: "/form/create" , pathMatch: "prefix"},
+    { path: "table/:category", component: TableComponent},
     { path: "table", component: TableComponent },
     { path: "", redirectTo: "/table", pathMatch: "full" },
     { path: "**", component: NotFoundComponent }

4c44920e5f7fc7ab14c73207ab4ee948c1b323ab
Rafael Freitas <oraffaudi@gmail.com>
2024-04-08 23:35:07 -0300

    applying the ripple effect

--- src/app/core/customButton.component.ts
+++ src/app/core/customButton.component.ts
@@ -7,10 +7,13 @@ import { Component, ElementRef, Input, ViewChild } from "@angular/core";
     styleUrls: ["customButton.component.scss"]
 })
 export class CustomButton {
+    
     @Input("themeColor")
     themeColor: string = "primary"
+
     @ViewChild("buttonTarget")
     button?: ElementRef
+    
     ngAfterViewInit() {
         this.button?.nativeElement.classList.add(`custom-button-${this.themeColor}`);
     }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatTableModule } from "@angular/material/table";
 import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatSortModule} from "@angular/material/sort"
+import { MatSortModule} from "@angular/material/sort";
+import { MatRippleModule } from "@angular/material/core";
 
-const features = [MatButtonModule, MatTableModule, MatPaginatorModule, MatSortModule];
+const features = [
+    MatButtonModule, 
+    MatTableModule, 
+    MatPaginatorModule, 
+    MatSortModule,
+    MatRippleModule
+];
 
 
 @NgModule({
